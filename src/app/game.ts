10689e9c7a0f2efa1e46abdab94a4315680e9270
Gabriel Landes <gabe.landes@digitaldefense.com>
2018-06-20 07:02:41 -0500

    let discover explore empty space

--- src/app/game.ts
+++ src/app/game.ts
@@ -1,67 +1,67 @@
 export interface GameData {
-    rows: number,
-    cols: number,
-    moves: number,
-    field: string[],
-    state: string[]
+  rows: number,
+  cols: number,
+  moves: number,
+  field: string[],
+  state: string[]
 }
 
 export class Game {
-    rows: number = 5;
-    cols: number = 5;
-    moves: number = 0;
-    field: string[] = [];
-    state: string[] = [];
+  rows: number = 8;
+  cols: number = 8;
+  moves: number = 0;
+  field: string[] = [];
+  state: string[] = [];
 
-    constructor (data?: GameData) {
-        if (data) Object.assign(this, data);
-        if (this.field.length !== this.rows * this.cols) this.initField();
+  constructor (data?: GameData) {
+    if (data) Object.assign(this, data);
+    if (this.field.length !== this.rows * this.cols) this.initField();
+  }
+
+  initField () {
+    this.field = new Array(this.rows * this.cols);
+    this.state = new Array(this.field.length).fill('H');
+
+    // lay the mines
+    for (let i = 0; i < this.field.length; i++) {
+      this.field[i] = Math.random() < .1 ? 'B' : '';
     }
+    // calculate adjacent mines
+    for (let i = 0; i < this.field.length; i++) {
+      if (this.field[i] === 'B') continue;
 
-    initField () {
-        this.field = new Array(this.rows * this.cols);
-        this.state = new Array(this.field.length).fill('H');
+      let c = 0;
+      let row = Math.floor((i)/this.cols);
+      let col = i % this.cols;
 
-        // lay the mines
-        for (let i = 0; i < this.field.length; i++) {
-            this.field[i] = Math.random() < .1 ? 'B' : '';
-        }
-        // calculate adjacent mines
-        for (let i = 0; i < this.field.length; i++) {
-            if (this.field[i] === 'B') continue;
-
-            let c = 0;
-            let row = Math.floor((i)/this.cols);
-            let col = i % this.cols;
-
-            // TL
-            if (row > 0 && col > 0 &&
-                this.field[(i - this.cols) - 1] === 'B') c++;
-            // TC
-            if (row > 0 &&
-                this.field[i-this.cols] === 'B') c++;
-            // TR
-            if (row > 0 && col < (this.cols-1) &&
-                this.field[(i - this.cols) + 1] === 'B') c++;
-            // ML
-            if (col > 0 &&
-                this.field[i - 1] === 'B') c++;
-            // MR
-            if (col < (this.cols - 1) &&
-                this.field[i + 1] === 'B') c++;
-            // BL
-            if (row < (this.rows - 1) &&
-                col > 0 &&
-                this.field[(i + this.cols) - 1] === 'B') c++;
-            // BC
-            if (row < (this.rows - 1) &&
-                this.field[i + this.cols] === 'B') c++;
-            // BR
-            if (row < (this.rows - 1) &&
-                col < (this.cols - 1) &&
-                this.field[(i + this.cols) + 1] === 'B') c++;
-            // set value as string
-            if (this.field[i] !== 'B') this.field[i] = String(c);
-        }
+      // TL
+      if (row > 0 && col > 0 &&
+        this.field[(i - this.cols) - 1] === 'B') c++;
+      // TC
+      if (row > 0 &&
+        this.field[i-this.cols] === 'B') c++;
+      // TR
+      if (row > 0 && col < (this.cols-1) &&
+        this.field[(i - this.cols) + 1] === 'B') c++;
+      // ML
+      if (col > 0 &&
+        this.field[i - 1] === 'B') c++;
+      // MR
+      if (col < (this.cols - 1) &&
+        this.field[i + 1] === 'B') c++;
+      // BL
+      if (row < (this.rows - 1) &&
+        col > 0 &&
+        this.field[(i + this.cols) - 1] === 'B') c++;
+      // BC
+      if (row < (this.rows - 1) &&
+        this.field[i + this.cols] === 'B') c++;
+      // BR
+      if (row < (this.rows - 1) &&
+        col < (this.cols - 1) &&
+        this.field[(i + this.cols) + 1] === 'B') c++;
+      // set value as string
+      if (this.field[i] !== 'B') this.field[i] = String(c);
     }
+  }
 }
--- src/app/minefield/minefield.component.ts
+++ src/app/minefield/minefield.component.ts
@@ -27,6 +27,49 @@ export class MinefieldComponent implements OnInit {
 
   discover (r,c) {
     let i = (r * this.game.cols) + c;
-    this.game.state[i] = this.game.field[i];
+    let val = this.game.field[i];
+    this.game.state[i] = val;
+    if (val === '0') this.explore(i);
+  }
+
+  explore (i) {
+    let row = Math.floor((i)/this.game.cols);
+    let col = i % this.game.cols;
+    // TL
+    if (row > 0 && col > 0 && this.isOpen(row-1,col-1)) {
+      this.discover(row-1,col-1);
+    }
+    // TC
+    if (row > 0 && this.isOpen(row-1,col)) {
+      this.discover(row-1,col);
+    }
+    // TR
+    if (row > 0 && col < (this.game.cols-1) && this.isOpen(row-1,col+1)) {
+      this.discover(row-1,col+1);
+    }
+    // ML
+    if (col > 0 && this.isOpen(row,col-1)) {
+      this.discover(row,col-1);
+    }
+    // MR
+    if (col < (this.game.cols - 1) && this.isOpen(row,col+1)) {
+      this.discover(row,col+1);
+    }
+    // BL
+    if (row < (this.game.rows - 1) && col > 0 && this.isOpen(row+1,col-1)) {
+      this.discover(row+1,col-1);
+    }
+    // BC
+    if (row < (this.game.rows - 1) && this.isOpen(row+1,col)) {
+      this.discover(row+1,col);
+    }
+    // BR
+    if (row < (this.game.rows - 1) && col < (this.game.cols - 1) && this.isOpen(row+1,col+1)) {
+      this.discover(row+1,col+1);
+    }
+  }
+
+  private isOpen (r,c): boolean {
+    return this.game.state[(r * this.game.cols) + c] === 'H';
   }
 }
